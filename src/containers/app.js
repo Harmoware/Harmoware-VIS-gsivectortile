567f655ddbe94cb52e580bab600bfded6fe57b70
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2021-02-24 09:27:09 +0900

    package update

--- src/containers/app.js
+++ src/containers/app.js
@@ -4,7 +4,7 @@ import {
 } from 'harmoware-vis';
 
 //react-map-gl@6で依存するmapbox-gl@2でmapboxApiAccessTokenが必須設定に変更
-const MAPBOX_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
+//const MAPBOX_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
 
 const MAPSTYLE1 = '../../json/std.json'
 const MAPSTYLE2 = '../../json/std_vertical.json'
@@ -63,7 +63,7 @@ class App extends Container {
         <div className="harmovis_area">
           <HarmoVisLayers
             viewport={viewport} actions={actions}
-            mapboxApiAccessToken={MAPBOX_TOKEN}
+            mapboxApiAccessToken=''　//react-map-gl@6 mapbox-gl@2 にアップデートする場合アクセストークン必須
             mapboxAddLayerValue=''
             mapStyle={mapStyle}
             layers={[
